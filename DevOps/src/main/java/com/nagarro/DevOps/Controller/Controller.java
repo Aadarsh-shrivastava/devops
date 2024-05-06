374bacf0b140add30d2e1c98e53947fb342d1f8b
Aadarsh-shrivastava <aadarshkhurai@gmail.com>
2024-05-06 18:44:21 +0530

    first.

--- DevOps/src/main/java/com/nagarro/DevOps/Controller/Controller.java
+++ DevOps/src/main/java/com/nagarro/DevOps/Controller/Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 	@RequestMapping("/Hello")
 public class Controller {
-	
+
 	@GetMapping("/")
 	public String getHello(){
 		return "Hello Spring";
--- DevOps/src/test/java/com/nagarro/DevOps/DevOpsApplicationTests.java
+++ DevOps/src/test/java/com/nagarro/DevOps/DevOpsApplicationTests.java
@@ -3,11 +3,20 @@ package com.nagarro.DevOps;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
 @SpringBootTest
 class DevOpsApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
+	@Test
+	void mainMethodRuns() {
+		
+		DevOpsApplication.main(new String[] {});
+	}
 }
