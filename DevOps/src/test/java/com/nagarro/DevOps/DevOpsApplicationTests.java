248c2953c579aee846102498eb7a77a20dcd5c67
Aadarsh-shrivastava <aadarshkhurai@gmail.com>
2024-05-07 23:04:28 +0530

    jenkins

--- DevOps/src/test/java/com/nagarro/DevOps/DevOpsApplicationTests.java
+++ DevOps/src/test/java/com/nagarro/DevOps/DevOpsApplicationTests.java
@@ -1,22 +1,29 @@
 package com.nagarro.DevOps;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.util.AssertionErrors.assertTrue;
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
 class DevOpsApplicationTests {
 
-	@Test
-	void contextLoads() {
 
+	@Test
+	void contextLoads(ApplicationContext context) {
+		assertNotNull(context);
 	}
 
 	@Test
-	void mainMethodRuns() {
-		
+	void mainMethodRuns(CapturedOutput output) {
 		DevOpsApplication.main(new String[] {});
+		assertTrue("Output doesn't contain 'Working'", output.toString().contains("Working"));
 	}
 }
